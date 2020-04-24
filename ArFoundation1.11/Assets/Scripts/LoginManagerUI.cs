46e4d579d859bc49ea969e7ca98357fbdad86c46
Aatish chaudhari <40951689+Aatish13@users.noreply.github.com>
2020-04-24 16:45:56 +0530

    loginpage added

---
+++ ArFoundation1.11/Assets/Scripts/LoginManagerUI.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoginManagerUI : MonoBehaviour
+{
+
+    public GameObject ARCanvas;
+    public GameObject LoginCanvas;
+
+    public GameObject HomePanel;
+    public GameObject LoginPanal;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ARCanvas.SetActive(false);
+        LoginCanvas.SetActive(true);
+        HomePanel.SetActive(true);
+        LoginPanal.SetActive(false);
+    }
+
+    public void GotoLogin()
+    {
+        HomePanel.SetActive(false);
+        LoginPanal.SetActive(true);
+    }
+
+    public void GotoDashBoard()
+    {
+        LoginPanal.SetActive(false);
+        LoginCanvas.SetActive(false);
+        ARCanvas.SetActive(true);
+    }
+
+    public void BackToHome() {
+        HomePanel.SetActive(true);
+        LoginPanal.SetActive(false);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
